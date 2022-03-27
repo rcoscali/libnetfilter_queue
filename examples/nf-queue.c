rcoscali/libnetfilter_queue

commit c3bada27b1db3044418733296cd00ba2e63b74fd

examples: print hwaddr attribute if present
There was a bug in 5.15 where HWADDR was included even if mac_header
start was same as network header.

In that case, userspace would get network header re-interpreted as a
mac address, i.e. hw->hw_addr[] set to network_header + 6 in ethernet
case.

Dump hwaddr if present.

Signed-off-by: Florian Westphal <fw@strlen.de>

examples/nf-queue.c
@@ -86,6 +86,24 @@ static int queue_cb(const struct nlmsghdr *nlh, void *data)
 	printf("packet received (id=%u hw=0x%04x hook=%u, payload len %u",
 		id, ntohs(ph->hw_protocol), ph->hook, plen);
 
+	if (attr[NFQA_HWADDR]) {
+		struct nfqnl_msg_packet_hw *hw = mnl_attr_get_payload(attr[NFQA_HWADDR]);
+		unsigned int hwlen = ntohs(hw->hw_addrlen);
+		const char *addr = hw->hw_addr;
+		unsigned int i;
+
+		printf(", hwaddr %02x", addr[0]);
+		for (i = 1; i < hwlen; i++) {
+			if (i >= sizeof(hw->hw_addr)) {
+				printf("[truncated]");
+				break;
+			}
+			printf(":%02x", (unsigned char)addr[i]);
+		}
+
+		printf(" len %u", hwlen);
+	}
+
 	/*
 	 * ip/tcp checksums are not yet valid, e.g. due to GRO/GSO.
 	 * The application should behave as if the checksums are correct.

commit b26df377c79fe5d7b58bef36fe5f38370a50955b

examples: fix compiler warning
Fixes: c3bada27b1db ("examples: print hwaddr attribute if present")
Signed-off-by: Duncan Roe <duncan_roe@optusnet.com.au>
Signed-off-by: Florian Westphal <fw@strlen.de>

examples/nf-queue.c
@@ -89,7 +89,7 @@ static int queue_cb(const struct nlmsghdr *nlh, void *data)
 	if (attr[NFQA_HWADDR]) {
 		struct nfqnl_msg_packet_hw *hw = mnl_attr_get_payload(attr[NFQA_HWADDR]);
 		unsigned int hwlen = ntohs(hw->hw_addrlen);
-		const char *addr = hw->hw_addr;
+		const unsigned char *addr = hw->hw_addr;
 		unsigned int i;
 
 		printf(", hwaddr %02x", addr[0]);