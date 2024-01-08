rcoscali/libnetfilter_queue

commit ffa83b5968b534f679bc34acc506801db3d28d58

add mangle functions for IPv4/TCP and IPv4/UDP
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

include/libnetfilter_queue/pktbuff.h
@@ -19,4 +19,8 @@ uint8_t *pktb_mac_header(struct pkt_buff *pktb);
 uint8_t *pktb_network_header(struct pkt_buff *pktb);
 uint8_t *pktb_transport_header(struct pkt_buff *pktb);
 
+int pktb_mangle(struct pkt_buff *pkt, unsigned int dataoff, unsigned int match_offset, unsigned int match_len, const char *rep_buffer, unsigned int rep_len);
+
+bool pktb_mangled(const struct pkt_buff *pktb);
+
 #endif

include/libnetfilter_queue/libnetfilter_queue_ipv4.h
@@ -7,6 +7,7 @@ struct iphdr;
 struct iphdr *nfq_ip_get_hdr(struct pkt_buff *pktb);
 int nfq_ip_set_transport_header(struct pkt_buff *pktb, struct iphdr *iph);
 void nfq_ip_set_checksum(struct iphdr *iph);
+int nfq_ip_mangle(struct pkt_buff *pkt, unsigned int dataoff, unsigned int match_offset, unsigned int match_len, const char *rep_buffer, unsigned int rep_len);
 int nfq_ip_snprintf(char *buf, size_t size, const struct iphdr *iph);
 
 #endif

include/libnetfilter_queue/libnetfilter_queue_tcp.h
@@ -13,6 +13,8 @@ struct ip6_hdr;
 void nfq_tcp_compute_checksum_ipv4(struct tcphdr *tcph, struct iphdr *iph);
 void nfq_tcp_compute_checksum_ipv6(struct tcphdr *tcph, struct ip6_hdr *ip6h);
 
+int nfq_tcp_mangle_ipv4(struct pkt_buff *pkt, unsigned int match_offset, unsigned int match_len, const char *rep_buffer, unsigned int rep_len);
+
 int nfq_tcp_snprintf(char *buf, size_t size, const struct tcphdr *tcp);
 
 #endif

include/libnetfilter_queue/libnetfilter_queue_udp.h
@@ -10,6 +10,8 @@ unsigned int nfq_udp_get_payload_len(struct udphdr *udph, struct pkt_buff *pktb)
 void nfq_udp_compute_checksum_ipv4(struct udphdr *udph, struct iphdr *iph);
 void nfq_udp_compute_checksum_ipv6(struct udphdr *udph, struct ip6_hdr *ip6h);
 
+int nfq_udp_mangle_ipv4(struct pkt_buff *pkt, unsigned int match_offset, unsigned int match_len, const char *rep_buffer, unsigned int rep_len);
+
 int nfq_udp_snprintf(char *buf, size_t size, const struct udphdr *udp);
 
 #endif

src/extra/checksum.c
@@ -10,6 +10,7 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <arpa/inet.h>
 #include <netinet/ip.h>
 #include <netinet/ip6.h>

src/extra/ipv4.c
@@ -10,6 +10,7 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <arpa/inet.h>
 #include <netinet/ip.h>
 
@@ -89,6 +90,24 @@ void nfq_ip_set_checksum(struct iphdr *iph)
 	iph->check = checksum(0, (uint16_t *)iph, iph_len);
 }
 EXPORT_SYMBOL(nfq_ip_set_checksum);
+
+int nfq_ip_mangle(struct pkt_buff *pkt, unsigned int dataoff,
+		  unsigned int match_offset, unsigned int match_len,
+		  const char *rep_buffer, unsigned int rep_len)
+{
+	struct iphdr *iph = (struct iphdr *) pkt->network_header;
+
+	if (!pktb_mangle(pkt, dataoff, match_offset, match_len,
+						rep_buffer, rep_len))
+		return 0;
+
+	/* fix IP hdr checksum information */
+	iph->tot_len = htons(pkt->len);
+	nfq_ip_set_checksum(iph);
+
+	return 1;
+}
+EXPORT_SYMBOL(nfq_ip_mangle);
 
 /**
  * nfq_pkt_snprintf_ip - print IPv4 header into buffer in iptables LOG format

src/extra/ipv6.c
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <stdbool.h>
 #include <arpa/inet.h>
 #include <netinet/ip6.h>
 

src/extra/pktbuff.c
@@ -11,6 +11,7 @@
 
 #include <stdlib.h>
 #include <string.h> /* for memcpy */
+#include <stdbool.h>
 
 #include <netinet/if_ether.h>
 #include <netinet/ip.h>
@@ -136,6 +137,67 @@ uint8_t *pktb_transport_header(struct pkt_buff *pktb)
 {
 	return pktb->transport_header;
 }
+
+static int pktb_expand_tail(struct pkt_buff *pkt, int extra)
+{
+	/* XXX: support reallocation case. */
+	pkt->len += extra;
+	pkt->tail = pkt->tail + extra;
+	return 0;
+}
+
+static int enlarge_pkt(struct pkt_buff *pkt, unsigned int extra)
+{
+	if (pkt->len + extra > 65535)
+		return 0;
+
+	if (pktb_expand_tail(pkt, extra - pktb_tailroom(pkt)))
+		return 0;
+
+	return 1;
+}
+
+int pktb_mangle(struct pkt_buff *pkt,
+		 unsigned int dataoff,
+		 unsigned int match_offset,
+		 unsigned int match_len,
+		 const char *rep_buffer,
+		 unsigned int rep_len)
+{
+	unsigned char *data;
+
+	if (rep_len > match_len &&
+	    rep_len - match_len > pktb_tailroom(pkt) &&
+	    !enlarge_pkt(pkt, rep_len - match_len))
+		return 0;
+
+	data = pkt->network_header + dataoff;
+
+	/* move post-replacement */
+	memmove(data + match_offset + rep_len,
+		data + match_offset + match_len,
+		pkt->tail - (pkt->network_header + dataoff +
+			     match_offset + match_len));
+
+	/* insert data from buffer */
+	memcpy(data + match_offset, rep_buffer, rep_len);
+
+	/* update pkt info */
+	if (rep_len > match_len)
+		pktb_put(pkt, rep_len - match_len);
+	else
+		pktb_trim(pkt, pkt->len + rep_len - match_len);
+
+	pkt->mangled = true;
+	return 1;
+}
+EXPORT_SYMBOL(pktb_mangle);
+
+bool pktb_mangled(const struct pkt_buff *pkt)
+{
+	return pkt->mangled;
+}
+EXPORT_SYMBOL(pktb_mangled);
 
 /**
  * @}

commit 396d187fc8684a49a867cdfeacf9c40baa99fdb8

include: pktbuff.h needs stdbool.h
Without #include <stdbool.h>, compilation will fail if pktbuff.h is
included early enough.

Fixes: ffa83b5968b5 ("add mangle functions for IPv4/TCP and IPv4/UDP")
Signed-off-by: Duncan Roe <duncan_roe@optusnet.com.au>
Signed-off-by: Florian Westphal <fw@strlen.de>

include/libnetfilter_queue/pktbuff.h
@@ -1,6 +1,8 @@
 #ifndef _PKTBUFF_H_
 #define _PKTBUFF_H_
 
+#include <stdbool.h>
+
 struct pkt_buff;
 
 struct pkt_buff *pktb_alloc(int family, void *data, size_t len, size_t extra);